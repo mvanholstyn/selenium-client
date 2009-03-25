bcc84df2fcdb3ef74ab1144dfc53465956bac260
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-03-25 13:28:06 -0400

    Make the remote control and associated rake task read the display to use from a config file.

--- lib/selenium/rake/remote_control_start_task.rb
+++ lib/selenium/rake/remote_control_start_task.rb
@@ -2,6 +2,13 @@ module Selenium
   module Rake
   
     class RemoteControlStartTask
+      config_file = File.join("config", "selenium.yml")
+      CONFIG = if File.exist?(config_file)
+        YAML.load_file(config_file).symbolize_keys
+      else
+        {}
+      end
+
       attr_accessor :port, :timeout_in_seconds, :background, 
                     :wait_until_up_and_running, :additional_args
       attr_reader :jar_file
@@ -29,6 +36,7 @@ module Selenium
           remote_control = Selenium::RemoteControl::RemoteControl.new("0.0.0.0", @port, @timeout_in_seconds)
           remote_control.jar_file = @jar_file
           remote_control.additional_args = @additional_args
+          remote_control.display = CONFIG[:display]
           remote_control.start :background => @background
           if @background && @wait_until_up_and_running
             puts "Waiting for Remote Control to be up and running..."
